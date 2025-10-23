e7bf8bea77bd2ee3cb8b0fbdfca79f93deea08da
jujubfs <julia.bitencourt@estudante.ifro.edu.br>
2025-10-23 11:34:31 -0400

    23-10-2025

--- src/app/produtos/page.js
+++ src/app/produtos/page.js
@@ -1,9 +1,40 @@
 import styles from './page.module.css';
 
 export default function Produtos() {
- return (
-    <div>
-        <h1>Produtos</h1>
-    </div>
- )
+    const produtos = [
+        { id: 1, nome: "Notebook", preco: 4500 },
+        { id: 2, nome: "Mouse", preco: 80 },
+        { id: 3, nome: "Teclado", preco: 150 },
+        { id: 4, nome: "Monitor", preco: 1200 },
+        { id: 5, nome: "Impressora", preco: 900 },
+        { id: 6, nome: "Cadeira Gamer", preco: 1800 },
+        { id: 7, nome: "Headset", preco: 300 },
+        { id: 8, nome: "Webcam", preco: 600 },
+        { id: 9, nome: "HD Externo", preco: 400 },
+        { id: 10, nome: "Mesa Digitalizadora", preco: 750 }
+    ];
+
+    return (
+        <div>
+            <h1>Produtos</h1>
+                <table className={styles.tbl}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>PREÇO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produtos.map((produto) => (
+                            <tr key={produto.id}>
+                                <td>{produto.id}</td>
+                                <td>{produto.nome}</td>
+                                <td>R$ {produto.preco.toFixed(2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+        </div>
+    )
 }
