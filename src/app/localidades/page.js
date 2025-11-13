92ce5b6995e0e9d383550f58813ae90b786c252a
jujubfs <julia.bitencourt@estudante.ifro.edu.br>
2025-11-13 11:40:57 -0400

    13-11-2025

--- src/app/localidades/page.js
+++ src/app/localidades/page.js
@@ -1,48 +1,88 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
 export default function Localidades() {
 
-    const getPaises = async () => {
+    const [ufs, setUfs] = useState([]);
+    const [pais, setPais] = useState('')
+    const [cidades, setCidades] = useState('');
+
+    const getUfs = async () => {
         try {
-            const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/paises?orderBy=nome');
+            const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome');
             if (!response.ok) {
                 throw new Error('Ocorreu um erro:', response.statusText);
             }
             const data = await response.json();
             console.log('dados', data);
+            setUfs(data)
 
         } catch (e) {
-            console.log('Ocorreu um erro ao buscar os países:', e.message);
+            console.log('Ocorreu um erro ao buscar os estados:', e.message);
         }
     }
 
+    const getCities = async () => {
+        try {
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${pais}/municipios`);
+            if (!response.ok) {
+                throw new Error('Ocorreu um erro:', response.statusText);
+            }
+            const data = await response.json();
+            console.log('dados', data);
+            setCidades(data)
+
+        } catch (e) {
+            console.log('Ocorreu um erro ao buscar as cidades:', e.message);
+        }
+    }
+
+    const selectUf = (ev) => {
+        setPais(ev.target.value);
+        console.log(pais)
+    }
+
     useEffect(() => {
-        getPaises();
+        getUfs();
     }, [])
 
     return (
         <div>
             <h1>Localidades</h1>
-            <table className={styles.tbl}>
+            {/* <table>
                     <thead>
                         <tr>
                             <th>ID</th>
+                            <th>SIGLA</th>
                             <th>NOME</th>
-                            <th>REGIÃO</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {paises.map(p => (
-                            <tr key={p.id}>
-                                <td>{p.id}</td>
-                                <td>{p.nome}</td>
-                                <td>{p.regiao}</td>
-                                </tr>
-                            ))}
+                        {ufs.map(uf => (
+                            <tr key={uf.id}>
+                                <td>{uf.id}</td>
+                                <td>{uf.sigla}</td>
+                                <td>{uf.nome}</td>
+                            </tr>
+                        ))}
                     </tbody>
-            </table>
+            </table> */}
+
+            <select
+                onClick={ev => selectUf(ev)}
+            >
+                {ufs.map(uf => (
+                    <option 
+                    value = {uf.id} 
+                    key = {uf.id}
+
+                    >
+                        {`${uf.sigla} - ${uf.nome}`}
+                    </option>
+                )
+                )}
+            </select>
         </div>
     )
 }
